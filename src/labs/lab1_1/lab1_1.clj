4b3509c224831657e0b3d34e3f802d8ecd3f8c7e
Svetlana Chernyavtseva <chernsvetl>
2023-09-28 01:09:43 +0700

    lab1-1 & lab1-3 done

--- src/labs/lab1_1/lab1_1.clj
+++ src/labs/lab1_1/lab1_1.clj
@@ -1,53 +1,34 @@
 (ns labs.lab1_1.lab1-1)
 
-(defn generate-word-with-one-symbol [word, alphabet] ;строим комбинации из одного символа, то есть тут список из комбинвций a,b,c
-
+(defn generate-word-with-one-symbol [word, alphabet]
   (if (= 0 (count alphabet))
     ()
-    (if (identical? (first alphabet) (first word))
-        (generate-word-with-one-symbol word (rest alphabet))
-        (->
-          (cons (cons (first alphabet) word) (generate-word-with-one-symbol word (rest alphabet)))
-          ;(println "----------------------------------------------------------")
-          ;(println word)
-          )
+    (if (= (first alphabet) (first word))
+      (generate-word-with-one-symbol word (rest alphabet))
+      (->
+        (cons (cons (first alphabet) word) (generate-word-with-one-symbol word (rest alphabet)))
+        ;(println "-----------------------------------")
         )
       )
+    )
   )
 
-
-(defn generate-words [words, alphabet]    ; идем по каждой комбинации (сначала a потом b потом c, после из комбинаций дальше ba, ba, ab и т д)
+(defn generate-words [words, alphabet]
   (if (= 0 (count words))
     ()
     (concat (generate-word-with-one-symbol (first words) alphabet) (generate-words (rest words) alphabet))
     )
   )
 
-(defn looping [words alphabet n]
-  (if (= n 0)
-    words
-    (looping (generate-words words alphabet) alphabet (dec n))
-    )
-  )
-
-
-(defn remove-duplicates-letters-in-alphabet [alphabet]  ;удаление дублирующихся символов в алфавите
-  (set alphabet)
-  )
-
 (defn generate-permutations [alphabet n]
-  (if (< n 0)
-    (println "incorrect n value")
-    (if (= n 0)
-        ()
-        (looping '(())  (remove-duplicates-letters-in-alphabet alphabet)  n)
+  (def list_ '(()))
+  (if (> n 0)
+    (generate-words (generate-permutations alphabet (dec n)) (set alphabet))
+    (if (< n 0)
+      (println "invalid n value")
+      list_
       )
     )
   )
 
-
-(run! println (generate-permutations '("a" "b" "c", "a", "b")  3)) ;run для переноса на каждую строку
-
-;(println (remove-duplicates '("a" "b" "c", "a")))
-;(generate-word-with-one-symbol "a" '["a" "b" "c"])
-
+(run! println (generate-permutations '("a" "b" "c" "a" "b")  3))
--- src/labs/lab1_2/lab1_2.clj
+++ src/labs/lab1_2/lab1_2.clj
@@ -1,1 +1,43 @@
 (ns labs.lab1-2.lab1-2)
+
+(defn generate-word-with-one-symbol [word alphabet]
+  (def list_ '())
+  ((defn generate-word-with-one-symbol-inner [alphabet-remain acc]
+     (if (= 0 (count alphabet-remain))
+       acc
+       (if (not= (first word) (first alphabet-remain))
+         (recur (rest alphabet-remain) (cons (cons (first alphabet-remain) word) acc))
+         (recur (rest alphabet-remain) acc)
+         )
+       )
+     ) alphabet list_)
+  )
+
+
+(defn generate-words [words alphabet]
+  (def list_ '())
+  ((defn generate-words-inner [words-remain acc]
+     (if (= 0 (count words-remain))
+       acc
+       (recur (rest words-remain) (concat acc (generate-word-with-one-symbol (first words-remain) alphabet)))
+       )
+     )
+   words list_)
+  )
+
+
+(defn generate-permutations [alphabet n]
+  (def list_ '(()))
+  ((defn generate-permutations-inner [n acc]
+     (if (< n 0)
+       (println "invalid n value")
+       (if (= 0 n)
+         acc
+         (recur (dec n) (generate-words acc (set alphabet)))
+         )
+       )
+     )
+   n list_)
+  )
+
+(run! println (reverse(generate-permutations '("a" "b" "c" "a" "b")  3)))
