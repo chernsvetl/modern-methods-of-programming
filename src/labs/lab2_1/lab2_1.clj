1507566b40d38261bee5eb3503566e223a1dd5e8
Svetlana Chernyavtseva <s.chernyavtseva@g.nsu.ru>
2023-10-22 23:48:19 +0700

    refactoring lab2_1

--- src/labs/lab2_1/lab2_1.clj
+++ src/labs/lab2_1/lab2_1.clj
@@ -14,11 +14,11 @@
     (+ (mem-func mem-func f a (- b 0.5)) (trapezia-square f (- b 0.5) b)))
   )
 
+(def rec-integral-mem (memoize rec-integral-no-mem))
+
 (defn integral-no-mem [x]
   (rec-integral-no-mem rec-integral-no-mem input-f 0 x)
   )
-
-(def rec-integral-mem (memoize rec-integral-no-mem))
 
 (defn integral-mem [x]
   (rec-integral-mem rec-integral-mem input-f 0 x)
@@ -31,6 +31,7 @@
   (time (integral-no-mem 99))
   ;(println (integral-no-mem 99))
   (time (integral-no-mem 101))
+  (time (integral-no-mem 100))
   ;(println (integral-no-mem 101))
   (println "-------------------------------")
   (time (integral-mem 100))
@@ -38,7 +39,10 @@
   (time (integral-mem 99))
   ;(println (integral-mem 99))
   (time (integral-mem 101))
+  (time (integral-mem 100))
   ;(println (integral-mem 101))
   )
 
 (-main)
+
+
