62233edac09f292e3ef6be473a13da0b2c1582a3
Svetlana Chernyavtseva <s.chernyavtseva@g.nsu.ru>
2023-12-24 15:59:17 +0700

    upd comments

--- src/labs/lab4/api.clj
+++ src/labs/lab4/api.clj
@@ -6,83 +6,84 @@
   (list :var name))
 
 (def const-true
-  ;"порождение константы true"
+  "порождение константы true"
   (list :true))
 
 (def const-false
-  ;"порождение константы false"
+  "порождение константы false"
   (list :false))
 
 (defn const-true? [expr]
-  ;"проверка того, что константа true"
+  "проверка того, что константа true"
   (= (first expr) :true))
 
 (defn const-false? [expr]
-  ;"проверка того, что константа false"
+  "проверка того, что константа false"
   (= (first expr) :false))
 
 (defn const? [expr]
-  ;"проверка типа для константы"
+  "проверка типа для константы"
   (or (const-false? expr)
       (const-true? expr)))
 
 (defn variable? [expr]
-  ;"проверка типа для переменной"
+  "проверка типа для переменной"
   (= (first expr) :var))
 
 (defn variable-name [v]
-  ;"получение значения для переменной"
+  "получение значения для переменной"
   (second v))
 
-(defn same-variables? [v1 v2]
-  ;"сравнение переменных"
+(defn same-variables?
+  "сравнение переменных"
+  [v1 v2]
   (and
     (variable? v1)
     (variable? v2)
     (= (variable-name v1) (variable-name v2))))
 
 (defn args [expr]
-  ;"список аргументов выражения"
+  "список аргументов выражения"
   (rest expr))
 
 (defn no [expr & rest]
-  ;"порождение отрицания"
+  "порождение отрицания"
   (cons :no (cons expr rest)))
 
 (defn no? [expr]
-  ;"проверка типа для отрицания"
+  "проверка типа для отрицания"
   (= :no (first expr)))
 
 (defn && [expr & rest]
-  ;"порождение конъюнкции"
+  "порождение конъюнкции"
   (cons :&& (cons expr rest)))
 
 (defn &&? [expr]
-  ;"проверка типа для конъюнкции"
+  "проверка типа для конъюнкции"
   (= :&&(first expr)))
 
 (defn || [expr & rest]
-  ;"порождение дизъюнкции"
+  "порождение дизъюнкции"
   (cons :|| (cons expr rest)))
 
 (defn ||? [expr]
-  ;"проверка типа для дизъюнкции"
+  "проверка типа для дизъюнкции"
   (= :|| (first expr)))
 
 (defn --> [expr & rest]
-  ;"порождение импликации"
+  "порождение импликации"
   (cons :--> (cons expr rest)))
 
 (defn -->? [expr]
-  ;"проверка типа для импликации"
+  "проверка типа для импликации"
   (= :-->(first expr)))
 
 (defn xor [expr & rest]
-  ; "порождение XOR"
+  "порождение XOR"
   (cons :xor (cons expr rest)))
 
 (defn xor? [expr]
-  ;"проверка типа для XOR"
+  "проверка типа для XOR"
   (= :xor(first expr)))
 
 (defn ↓↓ [expr & rest]
